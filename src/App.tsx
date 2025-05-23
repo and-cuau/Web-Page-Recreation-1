9917e6a7ef08184510d6b499450e830b8466d903
and-cuau <98066177+and-cuau@users.noreply.github.com>
2025-05-22 22:02:29 -0400

    Added parallax effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import { useEffect } from 'react';
 import Nav from "./components/Nav/nav.tsx";
 
 import Home from "./components/Home/home";
 
 function App() {
+  useEffect(() => {
+    const scroll = () => document.body.style.setProperty('--scroll', window.pageYOffset.toString());
+		window.addEventListener('scroll', scroll, false);
+		return () => window.removeEventListener('scroll', scroll);
+    
+  }, []); // empty dependency array = run once on mount
+
+
   return (
     <Router basename="/Web-Page-Recreation-1">
       <Nav></Nav>
